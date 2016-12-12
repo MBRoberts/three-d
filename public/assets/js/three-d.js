5c0cfab75508713c21c011014db7e2dd2776f38f
Ben Roberts <mikebenroberts@gmail.com>
2016-12-12 14:02:10 -0600

    :fire: :art: Cleanup and tracking
    Deleted extra files and added face tracking so user can control the scene with their face.

--- public/assets/js/three-d.js
+++ public/assets/js/three-d.js
@@ -1,15 +1,17 @@
-var camera, scene, light, renderer, bulbLight, bulbMat, ambientLight, object, loader, stats;
-var ballMat, cubeMat, floorMat, effect;
-var floatingDiv;
+var camera, scene, renderer, bulbLight, bulbMat;
+var ballMat, cubeMat, floorMat;
 var mouseX = 0, mouseY = 0;
 var spheres = [];
 
 var path = "/assets/textures/cube/skybox/";
 var format = '.jpg';
 var urls = [
-	path + 'px' + format, path + 'nx' + format,
-	path + 'py' + format, path + 'ny' + format,
-	path + 'pz' + format, path + 'nz' + format
+	path + 'px' + format, 
+	path + 'nx' + format,
+	path + 'py' + format, 
+	path + 'ny' + format,
+	path + 'pz' + format, 
+	path + 'nz' + format
 ];
 
 var textureCube = new THREE.CubeTextureLoader().load( urls );
@@ -40,14 +42,14 @@ var hemiLuminousIrradiances = {
 	"50000 lx (Direct Sun)": 50000,
 };
 
-var clock = new THREE.Clock();
-
 var params = {
 	shadows: true,
 	exposure: 0.68,
-	bulbPower: Object.keys( bulbLuminousPowers )[ 1 ],
+	bulbPower: Object.keys( bulbLuminousPowers )[1],
 	hemiIrradiance: Object.keys( hemiLuminousIrradiances )[0]
 };
+
+var clock = new THREE.Clock();
 
 function addGeometry() {
 
@@ -78,7 +80,7 @@ function addGeometry() {
 		map.repeat.set( 10, 24 );
 		floorMat.map = map;
 		floorMat.needsUpdate = true;
-	} );
+	});
 
 	textureLoader.load( "assets/textures/hardwood2_bump.jpg", function( map ) {
 		map.wrapS = THREE.RepeatWrapping;
@@ -87,7 +89,7 @@ function addGeometry() {
 		map.repeat.set( 10, 34 );
 		floorMat.bumpMap = map;
 		floorMat.needsUpdate = true;
-	} );
+	});
 
 	textureLoader.load( "assets/textures/hardwood2_roughness.jpg", function( map ) {
 		map.wrapS = THREE.RepeatWrapping;
@@ -96,7 +98,7 @@ function addGeometry() {
 		map.repeat.set( 10, 24 );
 		floorMat.roughnessMap = map;
 		floorMat.needsUpdate = true;
-	} );
+	});
 
 	cubeMat = new THREE.MeshStandardMaterial( {
 		roughness: 0.7,
@@ -112,7 +114,7 @@ function addGeometry() {
 		map.repeat.set( .3, .3 );
 		cubeMat.map = map;
 		cubeMat.needsUpdate = true;
-	} );
+	});
 
 	textureLoader.load( "assets/textures/brick_bump.jpg", function( map ) {
 		map.wrapS = THREE.RepeatWrapping;
@@ -121,7 +123,7 @@ function addGeometry() {
 		map.repeat.set( 1, 1 );
 		cubeMat.bumpMap = map;
 		cubeMat.needsUpdate = true;
-	} );
+	});
 
 	ballMat = new THREE.MeshStandardMaterial( {
 		color: 0xffffff,
@@ -133,50 +135,48 @@ function addGeometry() {
 		map.anisotropy = 4;
 		ballMat.map = map;
 		ballMat.needsUpdate = true;
-	} );
+	});
 
 	textureLoader.load( "assets/textures/planets/earth_specular_2048.jpg", function( map ) {
 		map.anisotropy = 4;
 		ballMat.metalnessMap = map;
 		ballMat.needsUpdate = true;
-	} );
+	});
 
-	var floorGeometry = new THREE.PlaneBufferGeometry( 20, 20 );
+	var floorGeometry = new THREE.PlaneBufferGeometry( 50, 50 );
 	var floorMesh = new THREE.Mesh( floorGeometry, floorMat );
-
 	floorMesh.receiveShadow = true;
 	floorMesh.rotation.x = -Math.PI / 2.0;
 	scene.add( floorMesh );
 
 	var geometry = new THREE.ConeBufferGeometry( 0.3, 0.7, 50 );
-	var meshMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, envMap :textureCube} );
-	var mesh = new THREE.Mesh(geometry, meshMaterial);	
-	mesh.position.set( 1, .85, 2 );
+	var mesh = new THREE.Mesh(geometry, floorMat);	
+	mesh.position.set( 1, .35, 2 );
 	mesh.castShadow = true;
 	mesh.receiveShadow = true;
 	scene.add( mesh );
 
 	var geometry2 = new THREE.BoxBufferGeometry( 0.75, 0.75, 0.75 );
-	var mesh2 = new THREE.Mesh( geometry2, meshMaterial );
-	mesh2.position.set(-0.75, 0.55, 1);
+	var mesh2 = new THREE.Mesh( geometry2, cubeMat );
+	mesh2.position.set(-2, 0.36, 1);
 	mesh2.castShadow = true;
 	mesh2.receiveShadow = true;
 	scene.add( mesh2 );
 
 	var geometry3 = new THREE.SphereBufferGeometry( 0.5, 32, 32);
 	var mesh3 = new THREE.Mesh( geometry3, ballMat );
-	mesh3.position.set(0.75, 0.65, -2);
+	mesh3.position.set(0.75, 0.5, -2);
 	mesh3.castShadow = true;
 	mesh3.receiveShadow = true;
 	scene.add( mesh3 );
 
 	var geometry4 = new THREE.SphereBufferGeometry( 0.1, 32, 32 );
-	var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube } );
+	var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube, metalness: 1, needsUpdate: true } );
 
 	for ( var i = 0; i < 500; i ++ ) {
 		var sphereMesh = new THREE.Mesh( geometry4, sphereMaterial );
 		sphereMesh.position.x = Math.random() * 50 - 15;
-		sphereMesh.position.y = Math.random() * 50 - 15;
+		sphereMesh.position.y = Math.random() * 50 + 15;
 		sphereMesh.position.z = Math.random() * 50 - 15;
 		sphereMesh.castShadow = true;
 		sphereMesh.receiveShadow = true;
@@ -185,51 +185,24 @@ function addGeometry() {
 		spheres.push( sphereMesh );
 	}
 
-	window.addEventListener('keyup', function(e){
-
-		function render1() {
-			requestAnimationFrame( render1 );
-			switch (e.keyCode){
-				case 37:
-					mesh.rotation.y -= 0.01;
-					mesh2.rotation.y -= 0.01;
-					mesh3.rotation.y -= 0.01;
-					break;
-				case 38:
-					mesh.rotation.x -= 0.01;
-					mesh2.rotation.x -= 0.01;
-					mesh3.rotation.x -= 0.01;
-					break;
-				case 39:
-					mesh.rotation.y += 0.01;
-					mesh2.rotation.y += 0.01;
-					mesh3.rotation.y += 0.01;
-					break;
-				case 40:
-					mesh.rotation.x += 0.01;
-					mesh2.rotation.x += 0.01;
-					mesh3.rotation.x += 0.01;
-					break;
-			}
-
-			renderer.render( scene, camera );
-		}
-		render1();
-	});
-	
 }
 
 function init() {
 	
 	var container = document.getElementById( 'container' );
-
-	stats = new Stats();
-	container.appendChild( stats.dom );
+	var bulbGeometry = new THREE.SphereGeometry( 0.05, 32, 16 );
+	var width = window.innerWidth || 2;
+	var height = window.innerHeight || 2;
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
+	renderer.physicallyCorrectLights = true;
+	renderer.gammaInput = true;
+	renderer.gammaOutput = true;
+	renderer.shadowMap.enabled = true;
+	renderer.toneMapping = THREE.ReinhardToneMapping;
+	container.appendChild( renderer.domElement );
 
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 100 );
 	camera.position.x = -4;
@@ -239,7 +212,6 @@ function init() {
 	scene = new THREE.Scene();
 	scene.background = textureCube;
 
-	var bulbGeometry = new THREE.SphereGeometry( 0.02, 16, 8 );
 	bulbLight = new THREE.PointLight( 0xffee88, 1, 100, 2 );
 	bulbMat = new THREE.MeshStandardMaterial( {
 		emissive: 0xffffee,
@@ -248,40 +220,18 @@ function init() {
 	});
 
 	bulbLight.add( new THREE.Mesh( bulbGeometry, bulbMat ) );
-	bulbLight.position.set( 0, 1.5, 0 );
+	bulbLight.position.set( 0, 1, 0 );
 	bulbLight.castShadow = true;
 	scene.add( bulbLight );
+	
 	hemiLight = new THREE.HemisphereLight( 0xddeeff, 0x0f0e0d, 0.02 );
 	scene.add( hemiLight );
 
-	light = new THREE.DirectionalLight( 0xffffff );
-	scene.add( light );
-
 	addGeometry();
 
-	renderer = new THREE.WebGLRenderer();
-	renderer.physicallyCorrectLights = true;
-	renderer.gammaInput = true;
-	renderer.gammaOutput = true;
-	renderer.shadowMap.enabled = true;
-	renderer.toneMapping = THREE.ReinhardToneMapping;
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	container.appendChild( renderer.domElement );
-
-	var width = window.innerWidth || 2;
-	var height = window.innerHeight || 2;
-
-	effect = new THREE.WebGLRenderer( renderer );
-	effect.setSize( width, height );
-
 	window.addEventListener( 'resize', onWindowResize, false );
-	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-	document.addEventListener( 'mouseover', onDocumentMouseMove, false );
 
-	floatingDiv = document.createElement( 'div' );
-	floatingDiv.className = 'floating';
-	container.appendChild( floatingDiv );
+	track();
 
 }
 
@@ -292,21 +242,53 @@ function onWindowResize() {
 
 	renderer.setSize( window.innerWidth, window.innerHeight );
 
-	effect.setSize( window.innerWidth, window.innerHeight );
-
 }
 
-function onDocumentMouseMove( event ) {
+function track( ) {
 
-	var windowHalfX = window.innerWidth / 2;
-	var windowHalfY = window.innerHeight / 2;
-	mouseX = ( event.clientX - windowHalfX )/50;
-	mouseY = ( event.clientY - windowHalfY )/50;
+	var viewport = document.getElementById('viewport');
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+	var tracker = new tracking.ObjectTracker('face');
+	tracker.setInitialScale(4);
+	tracker.setStepSize(2);
+
+	tracking.track('#video', tracker, { camera: true });
+
+	tracker.on('track', function(event) {
+    var maxRectArea = 0;
+    var maxRect;
+
+    event.data.forEach(function(rect) {
+		if (rect.width * rect.height > maxRectArea){
+		maxRectArea = rect.width * rect.height;
+		maxRect = rect;
+		}
+
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.strokeStyle = 'lime';
+		context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+		context.font = '11px Helvetica';
+		context.fillStyle = "#fff";
+		context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+		context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+    });
+
+    if(maxRectArea > 0) {
+		var rectCenterX = maxRect.x + (maxRect.width/2);
+		var rectCenterY = maxRect.y + (maxRect.height/2);
+		mouseX = -(rectCenterX - 160) / (window.innerWidth/320)/ 2;
+		mouseY = (rectCenterY - 120) / (window.innerHeight/240) / 2;
+    }
+  });
+
 
 }
 
 function animate() {
+
 	var previousShadowMap = false;
+	var timer = 0.001 * Date.now();
 
 	requestAnimationFrame( animate );
 
@@ -317,29 +299,30 @@ function animate() {
 	renderer.toneMappingExposure = Math.pow( params.exposure, 5.0 ); // to allow for very bright scenes.
 	renderer.shadowMap.enabled = params.shadows;
 	bulbLight.castShadow = params.shadows;
+	
 	if( params.shadows !== previousShadowMap ) {
+
 		ballMat.needsUpdate = true;
 		cubeMat.needsUpdate = true;
 		floorMat.needsUpdate = true;
 		previousShadowMap = params.shadows;
+
 	}
+	
 	bulbLight.power = bulbLuminousPowers[ params.bulbPower ];
 	bulbMat.emissiveIntensity = bulbLight.intensity / Math.pow( 0.02, 2.0 ); // convert from intensity to irradiance at bulb surface
 	hemiLight.intensity = hemiLuminousIrradiances[ params.hemiIrradiance ];
-	var time = Date.now() * 0.0005;
-	var delta = clock.getDelta();
-	bulbLight.position.y = Math.cos( time ) * 0.75 + 1.25;
-	// bulbLight.position.x = Math.cos( time ) * 1.6 + 1.5;
-	// bulbLight.position.z = Math.sin( time ) * 1.1 - .25;
+	
+	bulbLight.position.y = Math.cos( timer ) * 0.75 + 1.25;
+	// bulbLight.position.x = Math.cos( timer ) * 1.6 + 1.5;
+	// bulbLight.position.z = Math.sin( timer ) * 1.1 - .25;
+	
 	renderer.render( scene, camera );
-	stats.update();
-
-	var timer = 0.0001 * Date.now();
-
+	
 	for ( var i = 0, il = spheres.length; i < il; i ++ ) {
 		var sphere = spheres[ i ];
-		sphere.position.x = 10 * Math.cos( timer + i );
-		sphere.position.y = 10 * Math.sin( timer + i * 1.1 );
+		sphere.position.x = 10 * Math.cos( (timer/10) + i );
+		sphere.position.y = 10 * Math.sin( (timer/10) + i * 1.1 );
 	}
 
 }
